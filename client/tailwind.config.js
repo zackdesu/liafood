c88c6182c552cf9d39ea658a4098c786ea9db882
Wongso Wijaya <zarchxxx@gmail.com>
2024-03-21 17:35:25 +0700

    feat: colors add & font league spartan

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        LeagueSpartan: ["League Spartan", "sans-serif"],
+      },
+      colors: {
+        "blue-primary": "#304674",
+        "pink-primary": "#fb6f92",
+        "blue-secondary": "#98bad5",
+        "pink-secondary": "#ffc2d1",
+      },
+    },
   },
   plugins: [],
 };
